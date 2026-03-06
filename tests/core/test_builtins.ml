e9ffd55d15dc39cbd6cf1c1880c8a6b72f973a29
Bruno Rodrigues <bruno@brodrigues.co>
2026-03-06 11:12:33 +0100

    feat: update seq to support optional end and by arguments

--- tests/core/test_builtins.ml
+++ tests/core/test_builtins.ml
@@ -8,7 +8,11 @@ let run_tests _pass_count _fail_count _eval_string _eval_string_env test =
   test "assert false" "assert(false)" {|Error(AssertionError: "Assertion failed.")|};
   test "is_error on error" "is_error(1 / 0)" "true";
   test "is_error on value" "is_error(42)" "false";
-  test "seq" "seq(1, 3)" "[1, 2, 3]";
+  test "seq basic" "seq(1, 3)" "[1, 2, 3]";
+  test "seq end optional default" "seq(3)" "[1, 2, 3]";
+  test "seq named args" "seq(start = 1, end = 5, by = 2)" "[1, 3, 5]";
+  test "seq negative by" "seq(5, 1, -2)" "[5, 3, 1]";
+  test "seq end optional if by is provided" "seq(start = 3, by = 2)" "[3]";
   test "sum" "sum([1, 2, 3, 4, 5])" "15";
   test "map" "map([1, 2, 3], \\(x) x * x)" "[1, 4, 9]";
   print_newline ();
