dc59e56fcf3f9777951b8789dbce2873531e6813
Bruno Rodrigues <brodriguesco@protonmail.com>
2026-05-18 12:21:28 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
         # Use the Nix packages for the specified system
         pkgs = (import (builtins.fetchTarball {
           url    = "https://github.com/rstats-on-nix/nixpkgs/archive/${rstats-nix-date}.tar.gz";
-          sha256 = "sha256:1v89hz4ssiflry2jl1im9ld6fbq20cdn30f5xm5ka2nvmk42qqvs";
+          sha256 = "sha256:12yb71j0dishki0c78vxc1j3hdr1gj6p42db2ckkic3kj5pr4alr";
         }) { inherit system; }).extend (self: super: {
           lightgbm = super.lightgbm.overrideAttrs (old: {
             cudaSupport = false;
