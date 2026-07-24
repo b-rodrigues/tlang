68d28b9f3c4f5063b5685d6a051f30f751d7d36f
b-rodrigues <brodriguesco@protonmail.com>
2026-07-24 05:37:31 +0200

    test(testcraft): supply format argument to parse_datetime in expect_values test

--- tests/base/test_expect_ds_coverage.ml
+++ tests/base/test_expect_ds_coverage.ml
@@ -83,7 +83,7 @@ let run_tests _pass_count _fail_count _failures _eval_string _eval_string_env te
   test "expect_values stop"
     "df = to_dataframe([x: [1, 2, 3]]); expect_values(df, \"x\", [1, 2])" "STOP(";
   test "expect_values datetime pass"
-    "dt = parse_datetime(\"2023-01-01T10:00:00Z\"); df = to_dataframe([ts: [dt]]); expect_values(df, \"ts\", [dt])" "PASS";
+    "dt = parse_datetime(\"2023-01-01T10:00:00Z\", \"%Y-%m-%dT%H:%M:%SZ\"); df = to_dataframe([ts: [dt]]); expect_values(df, \"ts\", [dt])" "PASS";
   test "expect_values NA"
     "expect_values(NA, \"x\", [1, 2])" "HOLD(";
   test "expect_values Error"
